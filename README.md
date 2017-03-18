089deffc8436a09a1ba8f2e7d788ac8ea35ac65b
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-03-18 19:11:39 +0100

    Major rewrite. See README for details

---
+++ README.md
@@ -0,0 +1,36 @@
+# WordPress plugin builder
+
+## Description
+This is a shell script, which you can run on the command line and use
+[this boilerplate code](https://github.com/WPSwitzerland/wp-plugin-default) to automatically set up a new plugin.
+It provides you with all that you will need to start working on a WordPress plugin based around code using
+PHP namespaces.
+
+## Usage
+Run this shell script from the command line: ``./build-wordpress-plugin.sh``. The script will prompt you for the
+information it needs.
+
+### On Mac OS
+If you're going to use this bash script on Mac OS, you'll first need to replace the default version of ``sed`` with
+[the GNU version](https://www.gnu.org/software/sed/), so that all of the necessary regular expression functionality
+works. You can use [Homebrew](https://brew.sh/) to do this.
+
+```
+brew install gnu-sed --with-default-names
+```
+
+## Contributors
+Mark Howells-Mead | markweb.ch
+Mauro Bringolf | maurobringolf.ch
+Simon Bärlocher | sbaerlocher.ch
+
+## License
+Use this code freely, widely and for free. Provision of this code provides and implies no guarantee.
+
+Please respect the GPL v3 licence, which is available via http://www.gnu.org/licenses/gpl-3.0.html
+
+## Changelog
+
+### 0.2.0
+* Major rewrite, including the use of the plugin key as a basis for the file and folder name, as well as the PascalCase PHP namespace.
+* Issues remain when adding an http:// prefix to a URL, and the script entry is currently not validated. These will be addressed in an upcoming version.
---
+++ build-wordpress-plugin.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+
+function runPluginBuilder()
+{
+
+	# Ask for data
+    echo "Plugin key: lowercase with underscores (e.g. 'my_plugin')"
+    read PLUGIN_KEY
+
+    if test -z "$PLUGIN_KEY"
+    then
+        echo "You must specify a plugin key."
+        exit
+    fi
+
+    echo "Author name (e.g. 'Mark Mustermann')"
+    read PLUGIN_AUTHOR
+
+    echo "Author email address"
+    read AUTHOR_EMAIL
+
+    echo "Author URI (website address)"
+    read AUTHOR_URI
+
+    echo "Author's vendor name (e.g. 'CompanyName')"
+    read AUTHOR_NAMESPACE
+
+    echo "Plugin name (e.g. 'My great plugin')?"
+    read PLUGIN_NAME
+
+    echo "Plugin description"
+    read PLUGIN_DESCRIPTION
+
+    echo "Plugin URI (e.g. GitHub URL)"
+    read PLUGIN_URI
+
+	# Fetch the current base code from GitHub
+    git clone https://github.com/WPSwitzerland/wp-plugin-default $PLUGIN_KEY
+	rm -rf $PLUGIN_KEY/.git $PLUGIN_KEY/README.md
+    mv $PLUGIN_KEY/wp-plugin-default.php $PLUGIN_KEY/$PLUGIN_KEY.php
+    mv $PLUGIN_KEY/README_BLANK.md $PLUGIN_KEY/README.md
+
+	# Search and replace metadata and names
+
+    # Plugin key
+    find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/PLUGIN_KEY/$PLUGIN_KEY/g" {} +
+
+    # Plugin prefix (for non-namespace functionality)
+    find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/PLUGIN_PREFIX/$PLUGIN_KEY/g" {} +
+
+    # Text domain
+    find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/TEXT_DOMAIN/$PLUGIN_KEY/g" {} +
+
+    # Plugin domain (e.g. wpswitzerland/my_great_plugin)
+    # First convert to lowercase
+    PLUGIN_DOMAIN=$(echo "$AUTHOR_NAMESPACE/$PLUGIN_KEY" | sed -e 's/\(.*\)/\L\1/')
+    find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/PLUGIN_DOMAIN/$PLUGIN_DOMAIN/g" {} +
+
+    # Apply plugin key as pascal case namespace
+    PLUGIN_NAMESPACE=$(echo $PLUGIN_KEY | sed -e 's/_\([a-z]\)/\u\1/g' -e 's/^[a-z]/\u&/')
+    find $PLUGIN_KEY -type f -name "*.php" -exec sed -i "s/PLUGIN_NAMESPACE/$PLUGIN_NAMESPACE/g" {} +
+
+    # Plugin author name
+    if ! test -z "$PLUGIN_AUTHOR"
+    then
+        find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/PLUGIN_AUTHOR/$PLUGIN_AUTHOR/g" {} +
+    fi
+
+    # Plugin author email address
+    if ! test -z "$AUTHOR_EMAIL"
+    then
+        find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/AUTHOR_EMAIL/$AUTHOR_EMAIL/g" {} +
+    fi
+
+    # Plugin author website
+    if ! test -z "$AUTHOR_URI"
+    then
+        find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/AUTHOR_URI/$AUTHOR_URI/g" {} +
+    fi
+
+    # Plugin name
+    if ! test -z "$PLUGIN_NAME"
+    then
+		find $PLUGIN_KEY  -type f -name "*.*" -exec sed -i  "s/PLUGIN_NAME/$PLUGIN_NAME/g" {} +
+    fi
+
+    # Plugin description
+    if ! test -z "$PLUGIN_DESCRIPTION"
+    then
+    	find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/PLUGIN_DESCRIPTION/$PLUGIN_DESCRIPTION/g" {} +
+    fi
+
+    # Plugin author namespace (Usually a company name; used as the top-level PHP namespace)
+    if ! test -z "$AUTHOR_NAMESPACE"
+    then
+        find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/AUTHOR_NAMESPACE/$AUTHOR_NAMESPACE/g" {} +
+    fi
+
+    # Plugin's own website
+    if ! test -z "$PLUGIN_URI"
+    then
+        find $PLUGIN_KEY -type f -name "*.*" -exec sed -i "s/PLUGIN_URI/$PLUGIN_URI/g" {} +
+    fi
+
+}
+
+runPluginBuilder
+
+echo "You are good to go!"
+
